2d15e2f81c3d510c1458be374c2a140fef2b9297
qianliusi <qianliusi@gmail.com>
2020-05-29 21:13:54 +0800

    深度优先遍历复制

--- LeetCode/copyRandomList.js
+++ LeetCode/copyRandomList.js
@@ -59,3 +59,89 @@ var copyRandomList = function (head) {
     }
     return dfs(head)
 }
+
+/**
+ * 广度优先遍历复制
+ * @param {Node} head
+ * @returns {null|Node}
+ */
+var copyRandomList = function (head) {
+    let visited = {}
+    let bfs = function (head) {
+        if(!head) return null
+        let copy = new Node(head.val,null,null)
+        let queue = []
+        queue.push(head)
+        visited[head]=copy
+        while (queue.length) {
+            let node = queue.shift()
+            if (node.next && !visited[node.next]) {
+                visited[node.next] = new Node(node.next.val, null, null)
+                queue.push(node.next)
+            }
+            if (node.random && !visited[node.random]) {
+                visited[node.random] = new Node(node.random.val, null, null)
+                queue.push(node.random)
+            }
+            visited[node].next = visited[node.next]
+            visited[node].random = visited[node.random]
+        }
+        return copy
+    }
+    return bfs(head)
+}
+/**
+ * 直接遍历
+ * @param {Node} head
+ * @returns {null|Node}
+ */
+var copyRandomList = function (head) {
+    let getClonedNode = function (node) {
+        if(!node) return null
+        if(visited[node]) return visited[node]
+        visited[node] = new Node(node.val,null,null)
+        return visited[node]
+    };
+    if(!head) return head
+    let visited = {}
+    let oldNode = head
+    let newNode = new Node(oldNode.val,null,null)
+    visited[oldNode]=newNode
+    while (oldNode) {
+        newNode.next=getClonedNode(oldNode.next)
+        newNode.random=getClonedNode(oldNode.random)
+        oldNode=oldNode.next
+        newNode=newNode.next
+    }
+    return visited[head]
+}
+/**
+ * 优化遍历
+ * @param {Node} head
+ * @returns {null|Node}
+ */
+var copyRandomList = function (head) {
+    if(!head) return head
+    let cur = head
+    while (cur) {
+        let newNode = new Node(cur.val,null,null)
+        newNode.next=cur.next
+        cur.next = newNode
+        cur = newNode.next
+    }
+    cur = head
+    while (cur) {
+        cur.next.random = cur.random?cur.random.next:null
+        cur = cur.next.next
+    }
+    let curOld = head
+    let curNew = head.next
+    let newHead = head.next
+    while (curOld) {
+        curOld.next = curOld.next.next
+        curNew.next = curNew.next?curNew.next.next:null
+        curOld=curOld.next
+        curNew=curNew.next
+    }
+    return newHead
+}
