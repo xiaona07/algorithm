4e666b3979e0600d59c6413f31fc86a25b97e53b
qianliusi <qianliusi@gmail.com>
2020-05-30 18:12:09 +0800

    二叉搜索树与双向链表

---
+++ LeetCode/treeToDoublyList.js
@@ -0,0 +1,30 @@
+/**
+ * // Definition for a Node.
+ * function Node(val,left,right) {
+ *    this.val = val;
+ *    this.left = left;
+ *    this.right = right;
+ * };
+ */
+/**
+ * @param {Node} root
+ * @return {Node}
+ */
+var treeToDoublyList = function (root) {
+    let pre = null, head = null;
+    let dfs = function (cur) {
+        if(cur == null) return;
+        dfs(cur.left);
+        if(pre != null) pre.right = cur;
+        else head = cur;
+        cur.left = pre;
+        pre = cur;
+        dfs(cur.right);
+    }
+    if(root == null) return null;
+    dfs(root);
+    head.left = pre;
+    pre.right = head;
+    return head;
+
+};
