2c86f9dc6d25e423a554b2d37bd77941db8f8fb6
xiaona07 <queena5@163.comm>
2020-05-31 14:20:06 +0800

    二叉树序列化

--- LeetCode/serialize.js
+++ LeetCode/serialize.js
@@ -29,6 +29,65 @@ var serialize = function(root) {
     res+="]"
     return res
 };
+var serialize = function(root) {
+    if (!root) {
+        return "[]"
+    }
+    let stack = [root]
+    let stack1
+    let res = [[root.val]]
+    let res1
+    while (stack.length > 0) {
+        stack1 = []
+        res1 = []
+        while (stack.length > 0) {
+            let temp = stack.shift();
+            if (!temp) {
+                stack1.push(null);
+                res1.push(null);
+                stack1.push(null)
+                res1.push(null)
+            } else {
+                if(temp.left){
+                    stack1.push(temp.left);
+                    res1.push(temp.left.val);
+                }else {
+                    stack1.push(null);
+                    res1.push(null);
+                }
+                if(temp.right){
+                    stack1.push(temp.right)
+                    res1.push(temp.right.val)
+                }else {
+                    stack1.push(null)
+                    res1.push(null)
+                }
+            }
+
+        }
+        let result1 = res1.every((val) => {
+            return val === null
+        })
+        if (result1) {
+            break
+        }else {
+            res.push(res1)
+        }
+        let result2 = stack1.every((val) => {
+            return val === null
+        })
+        if (result2) {
+            break
+        }else {
+            stack = stack1
+        }
+    }
+    let arr = []
+    for (let i = 0; i < res.length; i++) {
+        arr = arr.concat(res[i])
+    }
+    return JSON.stringify(arr);
+};
 
 /**
  * Decodes your encoded data to tree.
@@ -57,7 +116,46 @@ var deserialize = function(data) {
     }
     return root;
 };
+var deserialize = function(data) {
+    let arr = JSON.parse(data);
+    console.log(arr)
+    if (arr.length <= 0) {
+        return null
+    }
+    let head = new TreeNode(arr[0])
+    let queue = [head]
+    let idx = 1
+    while (idx < arr.length) {
+        let now = queue.shift()
+        if (now == null) {
+            idx += 2
+            queue.push(null)
+            queue.push(null)
+        }else{
+            let left, right;
+            if(arr[idx] === null){
+                left = null
+                idx++
+            }else {
+                left = new TreeNode(arr[idx++])
+            }
+            if(arr[idx] === null){
+                right = null
+                idx++
+            }else {
+                right = new TreeNode(arr[idx++])
+            }
 
+            // let left = new TreeNode(arr[idx++])
+            // let right = new TreeNode(arr[idx++])
+            now.left = left
+            now.right = right
+            queue.push(left)
+            queue.push(right)
+        }
+    }
+    return head
+};
 
 function TreeNode(val) {
     this.val = val;
