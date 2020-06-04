09cac95a1dfb954f2d4b75aec79d2c4f79daf849
qianliusi <qianliusi@gmail.com>
2020-06-04 10:41:03 +0800

    quickSort partition2 fix

--- sort/quickSort.js
+++ sort/quickSort.js
@@ -7,8 +7,7 @@
 var quickSort = function (arr, l, r) {
     let len = r - l
     if (len > 1) {
-        let p = partition(arr, l, r)
-        console.log(p)
+        let p = partition2(arr, l, r)
         quickSort(arr, l, p)
         quickSort(arr, p + 1, r);
     }
@@ -29,25 +28,25 @@ var partition = function (arr, l, r) {
 }
 var partition2 = function (arr, left, right) {
     let pivot = arr[left]
-    let l = left + 1, r = right - 1
+    let l = left + 1, r = right
     while (l < r) {
         while (l < r && arr[l] <= pivot) {
             l++;
         }
-        while (r > l && arr[r] > pivot) {
+        while (r > l && arr[r-1] > pivot) {
             r--;
         }
         if (l < r) {
             swap(arr, l, r)
         }
     }
-    swap(arr, left, l)
-    return l
+    swap(arr, left, l-1)
+    return l-1
 }
 
 var swap = function (arr, i, j) {
     [arr[i], arr[j]] = [arr[j], arr[i]]
 }
-let array = [6, 5, 4, 3, 2, 1, 7, 8, 9]
+let array = [7,6,5,4,3,2,1]
 
 console.log(quickSort(array, 0, array.length))
