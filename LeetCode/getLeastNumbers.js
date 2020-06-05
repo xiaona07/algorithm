dd3494a692c60288cb72807579c4641b002cc070
xiaona07 <queena5@163.comm>
2020-06-05 18:01:23 +0800

    最小k个数  快速排序

--- LeetCode/getLeastNumbers.js
+++ LeetCode/getLeastNumbers.js
@@ -42,3 +42,48 @@ var getLeastNumbers2 = function(arr, k) {
     quickSort(arr, 0 , arr.length - 1)
     return arr.slice(0, k)
 };
+//大顶堆
+var getLeastNumbers3 = function(arr, k) {
+    let tree = arr.slice(0, k)
+    let sort = function (tree) {
+        let len = tree.length - 1
+        let index = Math.floor(tree.length / 2 - 1)
+        for (let i = index; i >= 0; i--) {
+            if(tree[i] < tree[2*i + 1]){
+                [tree[i], tree[2*i + 1]] =  [tree[2*i + 1], tree[i]]
+                down(tree, 2*i + 1)
+            }
+            if((2*i + 2) <= len &&  tree[i] < tree[2*i + 2]){
+                [tree[i], tree[2*i + 2]] =  [tree[2*i + 2], tree[i]]
+                down(tree, 2*i + 2)
+            }
+        }
+    }
+    let down = function (tree, idx) {
+        let len = tree.length - 1
+        // if(idx > len){
+        //     return
+        // }
+        if((2*idx + 1) <= len && tree[idx] < tree[2*idx + 1]){
+            [tree[idx], tree[2*idx + 1]] = [tree[2*idx + 1], tree[idx]]
+            return down(tree, 2*idx + 1)
+        }
+        if((2*idx + 2) <= len && tree[idx] < tree[2*idx + 2]){
+            [tree[idx], tree[2*idx + 2]] = [tree[2*idx + 2], tree[idx]]
+            return down(tree, 2*idx + 2)
+        }
+    };
+    sort(tree)
+    console.log(tree)
+    for (let i = k; i < arr.length; i++) {
+        if (tree[0] > arr[i]) {
+            tree[0] = arr[i]
+            down(tree, 0)
+        }
+    }
+    return tree
+};
+
+let arr = [0,1,1,1,4,5,3,7,7,8,10,2,7,8,0,5,2,16,12,1,19,15,5,18,2,2,22,15,8,22,17,6,22,6,22,26,32,8,10,11,2,26,9,12,9,7,28,33,20,7,2,17,44,3,52,27,2,23,19,56,56,58,36,31,1,19,19,6,65,49,27,63,29,1,69,47,56,61,40,43,10,71,60,66,42,44,10,12,83,69,73,2,65,93,92,47,35,39,13,75]
+let k = 75
+console.log(getLeastNumbers3(arr, k))
--- sort/quickSort.js
+++ sort/quickSort.js
@@ -48,5 +48,6 @@ var swap = function (arr, i, j) {
     [arr[i], arr[j]] = [arr[j], arr[i]]
 }
 let array = [7,6,5,4,3,2,1]
-
 console.log(quickSort(array, 0, array.length))
+let array2 = [3, 4, 2, 1, 7, 6, 8, 9, 2, 3, 4]
+console.log(quickSort(array2, 0, array2.length))
